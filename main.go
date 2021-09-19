1ef85247de18c9b8da23def990aef3aeb5d99987
dhanidev <husein@asiansigmatechnology.com>
2021-09-19 15:11:16 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bwastartup/user"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+
+	user := user.User{
+		Name:           "dhanidevSenchou",
+		Email:          "senchou@mugiwara.com",
+		PasswordHash:   "aAfrtKX54sbsga",
+		AvatarFileName: "mai.com",
+		Role:           "superuser",
+		Occupation:     "brogrammer",
+	}
+
+	userRepository.Save(user)
+
+}
